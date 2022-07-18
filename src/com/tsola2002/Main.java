deb1c4b2f18a5ce3ef5b31db17f04d0922c9d982
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-18 17:11:26 +0100

    Initial Commit

---
+++ src/com/tsola2002/Main.java
@@ -0,0 +1,9 @@
+package com.tsola2002;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  // write your code here
+        System.out.println("My first Java Program");
+    }
+}
