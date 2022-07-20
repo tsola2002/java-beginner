640cf04b1e6fb739593974b7a0b8e08794d70cda
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-20 15:21:09 +0100

    created some variables

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -1,9 +1,19 @@
 package com.tsola2002;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 	  // write your code here
+        // PRIMITIVE TYPES
+        int number = 100;
+        double decimal = 110.50;
+        String brand = "Adidas";
+
+        // REFERENCE TYPES
+        Date date = new Date();
+
         System.out.println("My first Java Program");
     }
 }
