ee8cdf23043c4f22e5c7063b6d550cd900b5da23
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-20 19:54:32 +0100

    running different string methods

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -1,9 +1,17 @@
 package com.tsola2002;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
 	  // write your code here
-        System.out.println("My first Java Program");
+        String name = "Amigoscode";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(1));
+        String code = "codes";
+        System.out.println(code.contains(code));
+        System.out.println(name.equals(code));
     }
 }
