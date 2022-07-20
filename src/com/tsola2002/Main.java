55d0679eb395b8ce260638f9067a6f06cfc91a08
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-20 19:21:12 +0100

    difference between primitive data type & reference data type

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -4,6 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 	  // write your code here
-        System.out.println("My first Java Program");
+
+        int a = 10;
+        int b = a;
+
+        System.out.println("a:" + a + "b: " + b);
+
+        Person alex = new Person("alex");
+        Person mariam = alex;
+
+        System.out.println("Before Changing name");
+        System.out.println(alex.name + " " + mariam.name );
+
+        //alex.name = "omatsola";
+        mariam.name = "mirabel";
+
+        System.out.println("After Changing name");
+        System.out.println(alex.name + " " + mariam.name );
+    }
+
+    // SAMPLE CLASS FOR REFERENCE
+    static class Person {
+        String name;
+
+        // CONSTRUCTOR
+        Person (String name) {
+            this.name = name;
+        }
     }
 }
