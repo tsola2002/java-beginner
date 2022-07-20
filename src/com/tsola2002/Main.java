5f47fa0e86425d32a4a2e4306d1a48f7e64a34a5
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-20 16:23:44 +0100

    creating primitive data types

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -4,6 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	  // write your code here
-        System.out.println("My first Java Program");
+        byte theByte = -128;
+        short theShort = 32_000;
+        int theInt = 787_838_334;
+        long theLong = 78783833432L;
+        float pi = 3.14F;
+        double doublePi = 3.145;
+        boolean isAdult = true;
+        char initialName = 'A';
+
+
+        System.out.println(theByte);
+        System.out.println(theShort);
+        System.out.println(theInt);
+        System.out.println(theLong);
+        System.out.println(pi);
+        System.out.println(doublePi);
+        System.out.println(isAdult);
+        System.out.println(initialName);
+
     }
 }
