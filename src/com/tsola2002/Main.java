16446ba28771ab41fb8477bc68f0e38a4f7d4209
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-21 17:13:39 +0100

    trying different arithmetic operations

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -3,7 +3,10 @@ package com.tsola2002;
 public class Main {
 
     public static void main(String[] args) {
-	  // write your code here
-        System.out.println("My first Java Program");
+	      // ARITHMETIC OPERATIONS
+        System.out.println(10 + 2 + 90);
+        System.out.println(10 -2);
+        System.out.println(10 *(2 + 7) - 4.5);
+        System.out.println(10 % 3);
     }
 }
