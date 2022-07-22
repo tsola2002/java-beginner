c0bf8d44a4dc3185a41a259e607c0ffc9d2657ac
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-22 15:08:54 +0100

    using if statements

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	  // write your code here
-        System.out.println("My first Java Program");
+        int age = 16;
+        if( age >= 18 ){
+            System.out.println("Hooray, I'm an adult");
+        } else if( age >= 16 && age < 18) {
+            System.out.println("I'm almost an adult");
+        } else {
+            System.out.println("I'm not an adult");
+        }
     }
 }
