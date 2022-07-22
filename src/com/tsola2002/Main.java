10af6825a84df6d63282a1fdb6c8567a6d803225
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-22 19:51:59 +0100

    using switch operators

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	  // write your code here
-        System.out.println("My first Java Program");
+        String gender = "FEMALE";
+
+        switch (gender) {
+            case "FEMALE":
+                System.out.println("I AM A FEMALE");
+                break;
+            case "MALE":
+                System.out.println("I AM A MALE");
+                break;
+            case "RATHER_NOT_SAY":
+                System.out.println("RATHER NOT SAY");
+                break;
+            default:
+                System.out.println("YOU HAVE NO GENDER");
+        }
+
+
     }
 }
