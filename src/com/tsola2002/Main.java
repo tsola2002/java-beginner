20a7af0d3eeb68ce70f556f46b9d5fd0cbf5ef57
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-22 15:32:19 +0100

    using ternary operators

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	  // write your code here
-        System.out.println("My first Java Program");
+        int age = 19;
+        String message = age <=18 ? "I'm not an adult" : "I'm a full grown adult now";
+        System.out.println(message);
     }
 }
