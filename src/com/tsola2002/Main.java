924f030ca69a656a30f747501838d0f4e92ae988
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-22 12:36:42 +0100

    trying out math.class operations

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	  // write your code here
-        System.out.println("My first Java Program");
+
+        // System.out.println("My first Java Program");
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.max(10, 6));
+        System.out.println(Math.min(6, 10));
+        System.out.println(Math.pow(5,2));
+        System.out.println(Math.PI);
     }
 }
