0baadcf4c4532f92a69daee0330ea282445c0371
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-25 11:07:08 +0100

    base code structured

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -3,7 +3,8 @@ package com.tsola2002;
 public class Main {
 
     public static void main(String[] args) {
-	  // write your code here
+
+	      // write your code here
         System.out.println("My first Java Program");
     }
 }
