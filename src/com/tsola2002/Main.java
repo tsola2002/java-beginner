6f7d46ae919d05af9320b28387a58bbbb5cfbc83
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-25 11:39:42 +0100

    creating arrays & array methods

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -1,10 +1,23 @@
 package com.tsola2002;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-	      // write your code here
-        System.out.println("My first Java Program");
+	      // ARRAYS
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        String[] letters = {  "a", "b", "c", "d", "e" };
+        System.out.println( "My first Java Program" );
+
+        int[] num = new int[3];
+        numbers[0] = 4;
+        numbers[1] = 1;
+        numbers[2] = 9;
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(letters));
     }
 }
