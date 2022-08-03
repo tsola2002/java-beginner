78c7b947e225b2e6e782b82e74bdda88acd85892
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-08-03 14:37:26 +0100

    creating & using lists

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -1,10 +1,31 @@
 package com.tsola2002;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        // LISTS
+        List numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(9);
+        numbers.add(100);
+        numbers.add(1000);
+
 	      // write your code here
-        System.out.println("My first Java Program");
+        //System.out.println(numbers);
+
+        //looping throught the list
+        //        for(Object number: numbers) {
+        //            System.out.println(number);
+        //        }
+
+
+        //loop through the list using foreach
+        numbers.forEach(System.out::println);
+
     }
 }
