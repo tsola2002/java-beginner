c49c9249fc8fe9974d543a8ca9abbeac0e33c824
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-08-24 19:10:09 +0100

    using break & continue

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -5,6 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
 	      // write your code here
-        System.out.println("My first Java Program");
+        String[] names = {"Anna", "Bella", "Charlie", "Dayo"};
+
+        for( String name:names ) {
+            // System.out.println(name);
+            // break;
+
+            if(name.equals("Dayo")){
+                break;
+            }
+            System.out.println(name);
+        }
+
     }
 }
