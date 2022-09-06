a7cad636e8dd3a72e8d37197124fb7bd9c534997
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-09-06 12:00:14 +0100

    using scanner class

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -1,10 +1,37 @@
 package com.tsola2002;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
 	      // write your code here
-        System.out.println("My first Java Program");
+          Scanner scanner = new Scanner(System.in);
+
+          System.out.println("What is your name?");
+
+          // take in the answer as input
+          String userName = scanner.nextLine();
+
+          // print the result out
+          System.out.println("Hello " + userName);
+
+        System.out.println("How old are you?");
+
+        int age = scanner.nextInt();
+
+        int year = LocalDate.now().minusYears(age).getYear();
+
+        System.out.println("You were born in " + year );
+
+        if(age < 18) {
+            System.out.println("You are not an adult");
+        } else {
+            System.out.println("you are an adult");
+        }
+
+
     }
 }
