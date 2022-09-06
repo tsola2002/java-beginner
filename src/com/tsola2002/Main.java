e36245822137da480886b8b02ed5eec01fc94e98
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-09-06 13:15:16 +0100

    user defined methods

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -5,6 +5,21 @@ public class Main {
     public static void main(String[] args) {
 
 	      // write your code here
-        System.out.println("My first Java Program");
+        // System.out.println("My first Java Program");
+
+        char[] letters = {'A','B','C','E','E','F'};
+        int count = countOccurences(letters, 'E');
+        System.out.println(count);
+
+    }
+
+    public static int countOccurences(char[] letters, char searchLetter){
+        int count = 0;
+        for(char letter: letters){
+            if( letter == searchLetter) {
+                count ++;
+            }
+        }
+        return count;
     }
 }
