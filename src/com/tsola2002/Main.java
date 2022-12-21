7d8716c9af6ecbb816fc8dfa8532a591bd191e0d
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-12-21 10:51:45 -0600

    creating the object

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -6,5 +6,31 @@ public class Main {
 
 	      // write your code here
         System.out.println("My first Java Program");
+
+        Lens lensOne = new Lens("Sony",
+                            "85mm",
+                                true);
+
+        Lens lensTwo = new Lens("Canon",
+            "40mm",
+            true);
+
+        Lens lensThree = new Lens("Panasonic",
+            "100mm",
+            false);
+
+
+    }
+
+    static class Lens{
+        String brand;
+        String focalLength;
+        Boolean isPrime;
+
+        Lens(String brand, String focalLength, Boolean isPrime){
+            this.brand = brand;
+            this.focalLength = focalLength;
+            this.isPrime = isPrime;
+        }
     }
 }
