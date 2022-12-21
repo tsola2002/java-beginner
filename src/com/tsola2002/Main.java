19107b85b715284f2ee3124119e9e76580c033bb
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-12-21 10:17:18 -0600

    sample built in class & cbject

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -5,6 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
 	      // write your code here
-        System.out.println("My first Java Program");
+        String passportnumber = new String("12345");
+        // passport number is an object of String class
+        // length() is a behaviour of string class
+        Integer length = passportnumber.length();
+
+        System.out.println(passportnumber);
+        System.out.println(length);
+
     }
 }
