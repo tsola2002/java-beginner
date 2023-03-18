b8870beb7e90c8b32e1c4db4a2651b0614be4458
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2023-03-18 09:38:47 +0100

    dealing with null pointer exceptions

--- src/com/tsola2002/Main.java
+++ src/com/tsola2002/Main.java
@@ -3,8 +3,31 @@ package com.tsola2002;
 public class Main {
 
     public static void main(String[] args) {
-
-	      // write your code here
-        System.out.println("My first Java Program");
+        // write your code here
+        //System.out.println("My first Java Program");
+       //String brand = null;
+        String brand = "Amigoscode";
+       try {
+           System.out.println(brand.toUpperCase());
+       } catch (NullPointerException e){
+           System.out.println(e);
+       }
     }
+
+     static class Cat {
+        private String name;
+
+         public String getName() {
+             return name;
+         }
+
+         public void setName(String name) {
+             this.name = name;
+         }
+
+         public Cat(String name) {
+            this.name = name;
+        }
+    }
+
 }
